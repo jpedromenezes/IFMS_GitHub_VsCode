0a2752683070d5386fab9c48c2aa65455a333a1e
Pedro Menezes <jjotamartinsmenezes31@gmail.com>
2024-03-26 21:47:30 -0400

    alteração do valor da variável

--- firstCode.js
+++ firstCode.js
@@ -1,8 +1,8 @@
 let x = 6;
 if (x > 5) {
-    console.log(`${x} é menor que 5`);
+    console.log(`${x} é maior que 5`);
 }else if(x == 5){
     console.log(`${x} é igual que 5`);
 }else{
-    console.log(`${x} é maior que 5`);
+    console.log(`${x} é menor que 5`);
 }
